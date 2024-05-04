db9eb7b6dc663d4deae6f4620fde020d4b58db88
JohnMoses-97 <760347john@gmail.com>
2024-05-04 22:51:46 +0530

    Mini Project

---
+++ projects/mini projects.py
@@ -0,0 +1,37 @@
+print ("welcome to the Quiz game !")
+
+playing = input("Do you want to play ?  ")
+if playing != "yes":
+    quit()
+print("Ok let's play :)" )
+
+question= input("What does ALU stand for? ")
+if question == "arithemetic logic unit":
+    print("correct")
+else:
+    print("incorrect")
+
+question= input("? ")
+if question == "":
+    print("correct")
+else:
+    print("incorrect")
+
+question= input("? ")
+if question == "":
+    print("correct")
+else:
+    print("incorrect")
+
+question= input("? ")
+if question == "":
+    print("correct")
+else:
+    print("incorrect")
+
+question= input("? ")
+if question == "":
+    print("correct")
+else:
+    print("incorrect")
+
